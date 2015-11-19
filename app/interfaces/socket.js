984c7223dbf1cc2f36bb8ffa02c0b5d22857c72e
Albert Hermida <alberthermida@gmail.com>
2015-11-19 11:27:38 -0500

    added some stuff for heroku

--- app/interfaces/socket.js
+++ app/interfaces/socket.js
@@ -1,4 +1,11 @@
 //SOCKET IO GLOBAL
-var socket = require('socket.io-client')('http://localhost:8080/');
+var isNode = (typeof process != 'undefined' && typeof process.versions != 'undefined' && typeof process.versions.node != 'undefined');
+var localSock;
+if (!isNode) {
+  localSock = window.location.hostname;
+} else {
+  localSock = "localhost:8080"
+}
+var socket = require('socket.io-client')(localSock);
 // Not to be used on the server
   module.exports = socket;
