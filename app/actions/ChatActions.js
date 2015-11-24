aee6c0d0bf811d35cfc5920e47ae8c09251ce61c
Albert Hermida <alberthermida@gmail.com>
2015-11-24 08:51:01 -0500

    heroku fix

--- app/actions/ChatActions.js
+++ app/actions/ChatActions.js
@@ -47,7 +47,7 @@ module.exports = {
   },
 
   loadMessages: function(thread) {
-    request.get('http://localhost:8080/api/c/' + thread)
+    request.get('https://localhost:8080/api/c/' + thread)
     .end(function(err, res) {
         if(err)
           console.log(err);
